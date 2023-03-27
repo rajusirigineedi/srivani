6882385dc1ed01c22365646e8cb1a21a4c464b6e
Sirigineedi Hemanth Pallapa Raju <sirigineedi@geekyants.com>
2023-03-27 22:50:21 +0530

    🚀 First commit

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <button style={{ backgroundColor: "red", pading: 100, width: 200 }}>
+      button s
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import Button from "@/Components/Button/Button";
+import React from "react";
+
+const test = (props) => {
+  return (
+    <div>
+      <h1>Test your components here..</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default test;
