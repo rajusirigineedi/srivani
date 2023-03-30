9fb359fa0154981971f97eb08a7768e76460eb02
ravi <mooninthedarknight1999@gmail.com>
2023-03-30 21:45:22 +0530

    Created DirectorCard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,19 @@
 import AppLayout from "@/Components/AppLayout/AppLayout";
 import "antd/dist/reset.css";
 import "@/styles/globals.css";
-
+import Head from "next/head";
 export default function MyApp({ Component, pageProps }) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <div>
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/1d4296eca1.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </div>
   );
 }
