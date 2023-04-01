d41936ee1299b9a1ec08c6fb2278934157db01f1
Raju Sirigineedi <75467803+rajusirigineedi@users.noreply.github.com>
2023-04-01 15:38:41 +0530

    Revert "#creating-reusable-card"

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,11 @@
 import AppLayout from "@/Components/AppLayout/AppLayout";
 import "antd/dist/reset.css";
 import "@/styles/globals.css";
-import Head from "next/head";
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Head>
-        <script
-          src="https://kit.fontawesome.com/1d4296eca1.js"
-          crossorigin="anonymous"
-        ></script>
-      </Head>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </div>
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
   );
 }
