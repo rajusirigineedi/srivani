6c762a8a7467b6e855f471fbf51968296e9d6dbc
Sirigineedi Hemanth Pallapa Raju <sirigineedi@geekyants.com>
2023-04-03 13:08:29 +0530

    🚀 Add dev strapi support (Installations)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,41 @@ import EventSection from "@/Components/Home/EventSection/EventSection";
 import FeatureSection from "@/Components/Home/FeatureSection/FeatureSection";
 import Landing from "@/Components/Home/Landing/Landing";
 import Testimonials from "@/Components/Home/Testimonials/Testimonials";
+import { useQuery, gql } from "@apollo/client";
 import { Space } from "antd";
 import React from "react";
+
+const QUERY = gql`
+  # Write your query or mutation here
+  query AllFolders {
+    imageFolders {
+      data {
+        attributes {
+          folder
+          title
+          images {
+            data {
+              attributes {
+                title
+                info
+                image {
+                  data {
+                    attributes {
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 const Home = (props) => {
+  const { data, loading, error } = useQuery(QUERY);
+  console.log(data, loading, error);
   return (
     <Space direction="vertical">
       <Landing />
